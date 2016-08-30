3b9a14a45995b392f7af4bbf93a7b8e55ab69565
amdudda <tk0654wm@minneapolis.edu>
2016-08-30 18:04:50 -0500

    Added one more book and created some static values in Main - need to fork to finish work on Main module.

--- Main.py
+++ Main.py
@@ -2,3 +2,13 @@
 
 # starting to create main menu
 # will push this new comment and then Brandon will demo merging for Marian
+
+# some static values for the menu options so we don't have to recode a bunch of stuff if we change the numbers
+OPTION_BYCOURSENAME = '1'
+OPTION_BYTITLE = '2'
+OPTION_BYISBN = '3'
+OPTION_BYDEPARTMENT = '4'
+OPTION_BYCOURSEID = '5'
+OPTION_QUIT = '6'
+
+# TODO build menu display & selection handler
