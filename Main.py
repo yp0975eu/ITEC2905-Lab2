22d5fcbc3fcf254121c9e478fdb56e596f4f8ca7
amdudda <tk0654wm@minneapolis.edu>
2016-08-30 19:57:01 -0500

    Implement main menu.  Only quit works, but it reports "not implemented" for other choices.

--- Main.py
+++ Main.py
@@ -12,3 +12,30 @@ OPTION_BYCOURSEID = '5'
 OPTION_QUIT = '6'
 
 # TODO build menu display & selection handler
+
+def showmenu():
+    # shows the option menu at launch
+    menu = "Select an option:\n"
+    menu += "1.\t Search by course name\n" + \
+           "2.\t Search by book title\n" + \
+            "3.\t Search by ISBN\n" + \
+            "4.\t Search by course department\n" + \
+            "5.\t Search by course ID\n" + \
+            "6.\t Quit\n"
+    print(menu)
+
+def do_menu():
+    # prints the menu and acts on the choice made
+    while True:
+        showmenu()
+        userchoice = raw_input("> ")
+        # act on choice
+        if userchoice == OPTION_QUIT:
+            # quit the program
+            exit()
+            break   # just being cautious so we don't leave an endless loop lingering.
+        else:
+            print("This option has not been implemented.")
+
+# BODY OF CODE
+do_menu()
