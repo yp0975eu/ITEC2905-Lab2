0a034e87adaf531fe2e07ee4cb9c85bdd26ec215
amdudda <tk0654wm@minneapolis.edu>
2016-09-01 09:18:45 -0500

    Finished option menu skeleton.

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ OPTION_BYDEPARTMENT = '4'
 OPTION_BYCOURSEID = '5'
 OPTION_QUIT = '6'
 
-# TODO build menu display & selection handler
+# FUNCTIONS USED BY MAIN METHOD
 
 def showmenu():
     # shows the option menu at launch
@@ -25,7 +25,8 @@ def showmenu():
     print(menu)
 
 def do_menu():
-    # prints the menu and acts on the choice made
+    # calls showmenu and acts on the choice made
+    # TODO build objects and methods to handle the user's selections.
     while True:
         showmenu()
         userchoice = raw_input("> ")
@@ -34,8 +35,19 @@ def do_menu():
             # quit the program
             exit()
             break   # just being cautious so we don't leave an endless loop lingering.
+        elif userchoice == OPTION_BYCOURSENAME:
+            print("This option to search by course name has not been implemented.")
+        elif userchoice == OPTION_BYTITLE:
+            print("This option to search by book title has not been implemented.")
+        elif userchoice == OPTION_BYISBN:
+            print("This option to search by book ISBN has not been implemented.")
+        elif userchoice == OPTION_BYDEPARTMENT:
+            print("This option to search by course department has not been implemented.")
+        elif userchoice == OPTION_BYCOURSEID:
+            print("This option to search by course ID has not been implemented.")
         else:
-            print("This option has not been implemented.")
+            print("You have not made a valid selection.  Pleae try again.")
 
 # BODY OF CODE
+print("Welcome to the used book browser!\n")
 do_menu()
