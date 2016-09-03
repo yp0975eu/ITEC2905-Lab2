c9496be91c1401b9c7f7d4cb0680f338443a17d9
marian <marian@email.com>
2016-09-03 12:29:35 -0500

    Add books object

---
+++ book.py
@@ -0,0 +1,36 @@
+class book():
+
+
+ def __init__(self, bookID, title, edition, author, ISBN, price, coursID, false=None):
+    # initialize a course object
+    self.ID = bookID
+    self.Title = title
+    self.edition = edition
+    self.author = author
+    self.isbn = ISBN
+    self.price = price
+    self.courseID = coursID
+    self.soldout = false
+# to check if book sold out and not availble  but I don't know  if it can work out
+    # right way to code
+    def sold_out(self):
+        price(self.soldout)
+        return self.soldout
+# to add new book on list but i don't know if it is working out
+def addbook(self, title, edition, author, ISBN, price):
+    add_book = book(title, edition, author, ISBN, price)
+#
+    def get_courseID(self):
+        return self.courseID
+
+    def get_title(self):
+        return self.title
+
+    def getISBN(self):
+        return self.ISBN
+
+
+def __str__(self):
+    # this lets us stringify a book's information
+    return self.ID + ": " + self.title + ", " + self.edition + ", " \
+           + self.author + ", " + self.ISBN + ", " + self.price
