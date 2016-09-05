773120598caed469114a670b01b02dc80e84be51
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-09-04 20:46:06 -0500

    Database.py -insert seed data, insert, select statemets. books.txt - reformat JSON

--- Database.py
+++ Database.py
@@ -1,48 +1,163 @@
 import sqlite3
+import json
 
 # DB Design
 # books ( id<primary key>, name, edition, price, course.id<foreign key>, users.username<foreign key> )
-# courses ( id<primary key>, num, dept.code<foreign key>)
+# courses ( id<primary key>, course, department)
 # users ( username<primary key> ) TODO: will probably add more to the user class
 
 
 class DB:
+    # Class variable shared by all instances of the DB class
+    # https://docs.python.org/2/tutorial/classes.html#class-and-instance-variables
+    # http://programmers.stackexchange.com/a/200529
+
+    # double underscore to provide data mangling in case of
+    __db_connection = None
+    __db_cursor = None
+
+    # variable starting with underscore to indicate non-public class variables
+    __db_name = 'textbook.db'
+    __seed_data_filename = 'books.txt'
 
     def __init__(self):
-        pass
+        try:
+            self.__db_connection = sqlite3.connect(self.__db_name)
+            self.__db_cursor = self.__db_connection.cursor()
+            self.init_db()
+        except sqlite3.Error as e:
+            print("Cannot connect to database.", e)
 
     # Initialize a database if there isn't one already
-    @staticmethod
-    def init_db():
-        db_name = 'textbook.db'
-        user_sql = 'CREATE TABLE  IF NOT EXISTS users  ( username VARCHAR(50) PRIMARY KEY);'
+    def init_db(self):
+        user_sql = 'CREATE TABLE IF NOT EXISTS users  ( username VARCHAR(8) PRIMARY KEY);'
         course_sql = '''CREATE TABLE IF NOT EXISTS courses(
-               id       INTEGER NOT NULL PRIMARY KEY,
-               name     TEXT NOT NULL,
-               dept     VARCHAR(4) NOT NULL
+               course     TEXT NOT NULL,
+               department     VARCHAR(4) NOT NULL,
+               PRIMARY KEY (course, department)
                );'''
+
+        # id removed because sqlite3 makes an id automatically
+        # id          INTEGER NOT NULL PRIMARY KEY,
+
         books_sql = '''CREATE TABLE  IF NOT EXISTS books  (
-            id          INTEGER NOT NULL  PRIMARY KEY,
-            name        TEXT NOT NULL,
+            title       TEXT NOT NULL,
+            author      TEXT NOT NULL,
             edition     INTEGER,
-            course_id   INTEGER NOT NULL,
-            user_name   TEXT NOT NULL,
-            FOREIGN KEY(course_id) REFERENCES courses(id),
-            FOREIGN KEY(user_name) REFERENCES users(username)
+            course      INTEGER NOT NULL,
+            username    TEXT NOT NULL,
+            isbn        TEXT NOT NULL,
+            price       REAL NOT NULL,
+            status      REAL NOT NULL,
+            FOREIGN KEY(course) REFERENCES courses(course),
+            FOREIGN KEY(username) REFERENCES users(username)
             );'''
 
         try:
-            conn = sqlite3.connect(db_name)
-            cur = conn.cursor()
+            cur = self.__db_cursor
             cur.execute(user_sql)
             cur.execute(course_sql)
             cur.execute(books_sql)
+            self.seed_data()
         except sqlite3.OperationalError as o_err:
-            print(o_err+"err")
+            print(o_err,"err")
         except sqlite3.DatabaseError as db_err:
             print(db_err)
-        finally:
-            conn.close()
-        pass
 
-    pass
+    def get_test_data_from_file(self):
+        # read example data from file and insert it into database
+        filename = self.__seed_data_filename
+        with open(filename, 'r') as file_obj:
+            raw_json = file_obj.read()
+        file_obj.close()
+        books_data = json.loads(raw_json)
+        return books_data
+
+    def seed_data(self):
+        books_json = self.get_test_data_from_file()
+        # for each book insert into database
+        # insert course
+        # insert users
+        # insert books
+        for entry in books_json:
+            course_row_id = self.insert_course(entry['course'], entry['department'])
+            self.insert_user(entry['username'])
+            self.insert_book((entry['title'], entry['author'], entry['edition'], course_row_id, entry['username'], entry['isbn'],
+                              entry['price'], entry['status']))
+
+    def insert_course(self, course, department):
+        insert_sql = 'INSERT INTO courses VALUES(?, ?);'
+        cursor = self.__db_cursor
+        conn = self.__db_connection
+        try:
+            cursor.execute(insert_sql, (course, department))
+            conn.commit()
+            return cursor.lastrowid
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Values: (" + course + " : " + department + ")")
+            return self.select_course(course, department)
+
+    def select_course(self, course, department):
+        insert_sql = 'SELECT ROWID, course, department FROM courses WHERE course=? and department=?;'
+        cursor = self.__db_cursor
+        try:
+            cursor.execute(insert_sql, (course, department))
+            # index 0 is the row ID
+            return cursor.fetchone()[0]
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Values: (" + course + " : " + department + ")")
+
+    def insert_user(self, username):
+        insert_sql = 'INSERT INTO users VALUES( ? );'
+        cursor = self.__db_cursor
+        conn = self.__db_connection
+        try:
+            # Need comma at end of username to make a single value a tuple, otherwise it is just a grouped expression
+            # http://stackoverflow.com/a/16856730
+            cursor.execute(insert_sql, (username,))
+            conn.commit()
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Value: (" + username + ")")
+
+    # parameter username is a string,
+    # returns a row
+    # row['ROWID','username']
+    def select_user(self, username):
+        insert_sql = 'SELECT ROWID, username FROM users WHERE username=?;'
+        cursor = self.__db_cursor
+        try:
+            cursor.execute(insert_sql, (username, ))
+            # index 0 is the row ID
+            return cursor.fetchone()[0]
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Values: (" + username + ")")
+
+    def insert_book(self, values):
+        insert_sql = 'INSERT INTO books VALUES(?, ?, ?, ?, ?, ?, ?, ? );'
+        cursor = self.__db_cursor
+        conn = self.__db_connection
+        try:
+            # Need comma at end of username to make a single value a tuple, otherwise it is just a grouped expression
+            # http://stackoverflow.com/a/16856730
+            cursor.execute(insert_sql, values)
+            conn.commit()
+            return cursor.lastrowid
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Value:", values, ")")
+            return cursor.lastrowid
+            # username is a string,
+
+    # returns a list of rows
+    # takes a field to search and a value to search for
+    # for example 'title', 'Microsoft'
+    def select_book(self, field, value):
+        sql = 'SELECT *, FROM books WHERE ?=%?%;'
+        cursor = self.__db_cursor
+        try:
+            cursor.execute(sql, (field, value,))
+            # index 0 is the row ID
+            return cursor.fetchall()
+        except sqlite3.DatabaseError as db_err:
+            print(db_err, "Values: (" + field + " " + value + ")")
+
+
