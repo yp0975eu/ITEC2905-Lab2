6c2a2ba06128446a3e264072f527717dcdefaeec
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-09-07 12:58:22 -0500

    Add tbl creation and seed checks

--- Database.py
+++ Database.py
@@ -7,6 +7,7 @@ import json
 # users ( username<primary key> ) TODO: will probably add more to the user class
 
 
+
 class DB:
     # Class variable shared by all instances of the DB class
     # https://docs.python.org/2/tutorial/classes.html#class-and-instance-variables
@@ -20,28 +21,96 @@ class DB:
     __db_name = 'textbook.db'
     __seed_data_filename = 'books.txt'
 
+    # To add a table, enter the name to this tuple, then create a method with
+    # make_table_<table_name> that contains the table sql,
+    # see make_table_users for an example
+    __table_names = ('books', 'users', 'db_meta', 'course')
+
     def __init__(self):
         try:
             self.__db_connection = sqlite3.connect(self.__db_name)
             self.__db_cursor = self.__db_connection.cursor()
-            self.init_db()
+
+            # checks if our table inside __table_names exist, if not it creates them
+            self.check_for_tables()
+
+            # check if out tables are empty.
+            if self.tables_need_data():
+                self.seed_data()
+
         except sqlite3.Error as e:
             print("Cannot connect to database.", e)
 
-    # Initialize a database if there isn't one already
-    def init_db(self):
-        db_meta_sql = 'CREATE TABLE IF NOT EXISTS db_meta  ( version REAL, last_update DATETIME);'
-        user_sql = 'CREATE TABLE IF NOT EXISTS users  ( username VARCHAR(8) PRIMARY KEY);'
-        course_sql = '''CREATE TABLE IF NOT EXISTS courses(
+    def check_for_tables(self):
+        for table in self.__table_names:
+            if not self.table_exist(table):
+                try:
+                    # this calls the method named: self.make_table_<table_name>
+                    getattr(self, 'make_table_'+table)()
+                except AttributeError as a_error:
+                    print("Error making table. "
+                          "Does table '{}' have a corresponding method 'make_table_{}()' ?".format(table, table))
+                    print(a_error)
+
+    def tables_need_data(self):
+        #  we are only checking the books table,
+        #  but in the future can check multiple tables to seed
+        #  If there is are books then seed the db
+        row_count = self.does_table_have_data()
+        return row_count <= 0
+
+    # returns true if table exists
+    def table_exist(self, table_name):
+        cur = self.__db_cursor
+
+        # SQL from http: // stackoverflow.com / a / 8827554
+        check_table = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name = ?;"
+        try:
+            values = table_name,
+            cur.execute(check_table, values)
+            count = cur.rowcount
+            return count == 1
+        except sqlite3.OperationalError as o_err:
+            print('Operational Error', o_err)
+        except sqlite3.DatabaseError as db_err:
+            print('Table Exists Error', db_err)
+
+    # Returns row count
+    # returns 0 if there is not data
+    def does_table_have_data(self):
+        cur = self.__db_cursor
+        # table names cannot be parametrized arguments?
+        # http://stackoverflow.com/questions/3247183/variable-table-name-in-sqlite
+        sql = "SELECT count(*) FROM books;"
+        try:
+            # execute takes a tuple for arg2
+            cur.execute(sql)
+            return cur.rowcount
+        except sqlite3.OperationalError as o_err:
+            print("Table data check Operational Error", o_err)
+        except sqlite3.DatabaseError as db_err:
+            print(db_err)
+
+    def make_table_db_meta(self):
+        sql = 'CREATE TABLE IF NOT EXISTS db_meta  ( version REAL, last_update DATETIME);'
+        self.make_table(sql)
+
+    def make_table_users(self):
+        sql = 'CREATE TABLE IF NOT EXISTS users  ( username VARCHAR(8) PRIMARY KEY);'
+        self.make_table(sql)
+
+    def make_table_course(self):
+        sql = '''CREATE TABLE IF NOT EXISTS courses(
                course     TEXT NOT NULL,
                department     VARCHAR(4) NOT NULL,
                PRIMARY KEY (course, department)
                );'''
+        self.make_table(sql)
 
+    def make_table_books(self):
         # id removed because sqlite3 makes an id automatically
         # id          INTEGER NOT NULL PRIMARY KEY,
-
-        books_sql = '''CREATE TABLE  IF NOT EXISTS books  (
+        sql = '''CREATE TABLE  IF NOT EXISTS books  (
             title       TEXT NOT NULL,
             author      TEXT NOT NULL,
             edition     INTEGER,
@@ -53,18 +122,16 @@ class DB:
             FOREIGN KEY(course) REFERENCES courses(course),
             FOREIGN KEY(username) REFERENCES users(username)
             );'''
+        self.make_table(sql)
 
+    def make_table(self, sql):
         try:
             cur = self.__db_cursor
-            cur.execute(user_sql)
-            cur.execute(course_sql)
-            cur.execute(books_sql)
-            cur.execute(db_meta_sql)
-            self.seed_data()
+            cur.execute(sql)
         except sqlite3.OperationalError as o_err:
-            print(o_err,"err")
+            print("Error Making Table", o_err)
         except sqlite3.DatabaseError as db_err:
-            print(db_err)
+            print("Database Error while making table", db_err)
 
     def get_test_data_from_file(self):
         # read example data from file and insert it into database
@@ -96,8 +163,9 @@ class DB:
             conn.commit()
             return cursor.lastrowid
         except sqlite3.DatabaseError as db_err:
-            print(db_err, "Values: (" + course + " : " + department + ")")
-            return self.select_course(course, department)
+            row_id = self.select_course(course, department)
+            print("WARNING: Insert Course Failed. Course already exists, use ROWID: {} for ({}-{})".format(row_id, department, course))
+            return row_id
 
     def select_course(self, course, department):
         insert_sql = 'SELECT ROWID, course, department FROM courses WHERE course=? and department=?;'
@@ -107,7 +175,7 @@ class DB:
             # index 0 is the row ID
             return cursor.fetchone()[0]
         except sqlite3.DatabaseError as db_err:
-            print(db_err, "Values: (" + course + " : " + department + ")")
+            print(db_err, "Course Selection Failed: (" + course + " : " + department + ")")
 
     def insert_user(self, username):
         insert_sql = 'INSERT INTO users VALUES( ? );'
@@ -119,7 +187,7 @@ class DB:
             cursor.execute(insert_sql, (username,))
             conn.commit()
         except sqlite3.DatabaseError as db_err:
-            print(db_err, "Value: (" + username + ")")
+            print("WARNING: User Insertion Failed for {} user already exists".format(username))
 
     # parameter username is a string,
     # returns a row
