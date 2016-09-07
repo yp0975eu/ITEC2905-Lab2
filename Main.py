4c6f28b03f68c296aacd3fc91066a3e8556d2ad4
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-09-07 15:44:14 -0500

    added search function and a couple TODO notes

--- Main.py
+++ Main.py
@@ -1,31 +1,43 @@
 from Database import DB
+from Search import Search
 
 # initialize DB object, seeding database if necessary.
 db = DB()
-
+search = Search(db)
 # some static values for the menu options so we don't have to recode a bunch of stuff if we change the numbers
 
-OPTION_BYCOURSENAME = '1'
+OPTION_BYCOURSENUMBER = '1'
 OPTION_BYTITLE = '2'
 OPTION_BYISBN = '3'
-OPTION_BYDEPARTMENT = '4'
-OPTION_BYCOURSEID = '5'
-OPTION_QUIT = '6'
+OPTION_LISTDEPARTMENTS = '4'
+OPTION_BYDEPARTMENT = '5'
+OPTION_BYCOURSEID = '6'
+OPTION_QUIT = '7'
 
 # FUNCTIONS USED BY MAIN METHOD
 
 
 def showmenu():
     # shows the option menu at launch
-    menu = "Select an option:\n"
-    menu += "1.\t Search by course name\n" + \
-           "2.\t Search by book title\n" + \
-            "3.\t Search by ISBN\n" + \
-            "4.\t Search by course department\n" + \
-            "5.\t Search by course ID\n" + \
-            "6.\t Quit\n"
-    print(menu)
+    menu = ['Select an option:',
+            'Search by course number',
+            'Search by book title',
+            'Search by ISBN',
+            'List departments',
+            'Search by department',
+            'Quit']
+    count = 0
+    for option in menu:
+        if count == 0:
+            print(option)
+        else:
+            print("{}.\t{}".format(count, option))
+        count += 1
 
+
+def print_results(results):
+    for row in results:
+        print(row)
 
 def do_menu():
     # calls showmenu and acts on the choice made
@@ -38,16 +50,40 @@ def do_menu():
             # quit the program
             exit()
             break   # just being cautious so we don't leave an endless loop lingering.
-        elif userchoice == OPTION_BYCOURSENAME:
-            print("This option to search by course name has not been implemented.")
+        elif userchoice == OPTION_BYCOURSENUMBER:
+            course_number = input("Enter course number: ")
+            # TODO Validate course numbers
+            # if Course.isValid(course_number)
+            results = search.by_course_num(course_number)
+            if results:
+                print_results(results)
+            else:
+                print("No matches, try a department search.")
         elif userchoice == OPTION_BYTITLE:
+            title = input("Enter book title: ")
+            results = search.by_book_title(title)
+            if results:
+                print_results(results)
+            else:
+                print("No matches, try a department search.")
             print("This option to search by book title has not been implemented.")
         elif userchoice == OPTION_BYISBN:
+            # TODO: Change format of isbns to not inlude dashes, or add dashes on the backend for searching
             print("This option to search by book ISBN has not been implemented.")
         elif userchoice == OPTION_BYDEPARTMENT:
+            dept = input("Enter department ID ")
+            results = search.by_department(dept)
+            if results:
+                print_results(results)
+            else:
+                print("No matches")
             print("This option to search by course department has not been implemented.")
-        elif userchoice == OPTION_BYCOURSEID:
-            print("This option to search by course ID has not been implemented.")
+        elif userchoice == OPTION_LISTDEPARTMENTS:
+            results = search.list_departments()
+            if results:
+                print_results(results)
+            else:
+                print("No departments to choose from , something is fishy :/")
         else:
             print("You have not made a valid selection.  Please try again.")
 
