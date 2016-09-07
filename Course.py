f7c987593b11d35f176233a32a1cea2eb62207fe
amdudda <tk0654wm@minneapolis.edu>
2016-09-07 17:59:37 -0500

    Add insert and delete methods for Course objects.

--- Course.py
+++ Course.py
@@ -4,11 +4,11 @@ DEV = True; # flag to switch between prod and dev.
 
 class Course():
 
-    def __init__(self, CourseID, CourseName, CourseDepartment):
+    def __init__(self, cNum, CourseDepartment):
         # initialize a course object
-        self.ID = CourseID
-        self.Name = CourseName
+        self.CourseNum = cNum
         self.Department = CourseDepartment
+        self.ID = CourseDepartment + cNum
 
     # plus some simple getters and setters
     # omitting 'course' from method names because syntax is Course.getFoo().
@@ -16,14 +16,42 @@ class Course():
         return self.ID
 
     def getName(self):
-        return self.Name
+        return self.CourseNum
 
     def getDepartment(self):
         return self.Department
 
+    def insert(self):
+        try:
+            conn = sqlite3.connect("textbook.db")
+            qryString = "INSERT INTO courses VALUES (?,?);"
+            conn.execute(qryString, (self.CourseNum, self.Department))
+            conn.commit()
+        except sqlite3.Error as e:
+            # report an error and roll back the changes
+            print("Unable to insert record into course table: " + str(self))
+            if DEV: traceback.print_exc()
+            conn.rollback()
+        finally:
+            conn.close()
+
+    def delete(self):
+        try:
+            conn=sqlite3.connect("textbook.db")
+            qryString = "DELETE FROM courses WHERE course=? AND department=?;"
+            conn.execute(qryString, (self.CourseNum, self.Department))
+            conn.commit()
+        except sqlite3.Error as e:
+            # report an error and roll back the changes
+            print("Unable to deete record from course table: " + str(self))
+            if DEV: traceback.print_exc()
+            conn.rollback()
+        finally:
+            conn.close()
+
     def __str__(self):
         # this lets us stringify a course's information
-        return self.ID + ": " + self.Department + ", " + self.Name
+        return self.ID + ": " + self.Department + ", " + self.CourseNum
 
 # and some generic methods to pull data out of the database
 
@@ -38,10 +66,10 @@ def getByDepartment(Department):
 
         # build a list of courses - need to print rows in order to figure out the order the fields are returned
         for row in cursor:
-            cName = row[0]
+            cNum = row[0]
             cDept = row[1]
-            cID = cDept + cName
-            course = Course(cID,cName,cDept)
+            # cID = cDept + cNum
+            course = Course(cNum,cDept)
             courselist.append(course)
 
     except sqlite3.Error as e:
@@ -72,8 +100,8 @@ def getByCourseID(CourseID):
         # grab the data and create a course object
         cName = cursor[0]
         cDept = cursor[1]
-        cID = cDept + cName
-        course = Course(cID,cName,cDept)
+        #cID = cDept + cName
+        course = Course(cName,cDept)
     except sqlite3.Error as e:
         # report the error and rollback
         print('Unable to retrieve data from database.')
